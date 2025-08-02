cfcf9dfd73a57d17db863906cd0859e326d114e1
SaleteReboucas <salete.guimaraes@yahoo.com.br>
2025-08-01 22:26:07 -0300

    Resolucao questao 02

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+package questao02
+
+import java.util.Scanner;                                                    
+                                                                                                                                                                                                                                             
+public class Principal {
+
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("Digite seu peso: ");
+       float peso = entrada.nextfloat();
+       System.out.println("Digite sua altura: ");
+       float altura = entrada.nextfloat();
+
+       if()
+    }
+}
