b15e0eda5ad1e23a0484c6e5c224b746ce39830a
SaleteReboucas <salete.guimaraes@yahoo.com.br>
2025-08-05 19:25:30 -0300

    Conclusao questao04

---
+++ src/questao04/Principal.java
@@ -0,0 +1,21 @@
+package questao04;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System .out.println("Digite a senha: ");
+
+        int senha = entrada.nextInt();
+        int senhaCorreta = 1234;
+
+        if(senha == senhaCorreta){
+            System.out.println("ACESSO PERMITIDO ");
+       }else{
+         System.out.println("ACESSO NEGADO ");
+       }
+        }
+    }
+
